fe1ccb65f93c5ae28d03982dd841a1d8c64fa0d2
Carlos-Marte <carlosmartedev@hotmail.com>
2022-09-10 19:14:23 -0400

    Funcionalidades de conteo y de eliminar tareas

--- scripts/script.js
+++ scripts/script.js
@@ -4,48 +4,96 @@ const fatherDiv = document.querySelector("#div-flex");
 const inputText = document.querySelector("#input-text");
 const addButton = document.querySelector("#add-button");
 
-addButton.addEventListener('click', crearTarea);
+const pendingTasks = document.querySelector("#p-pending-tasks");
+const completeTasks = document.querySelector("#p-complete-tasks");
 
-//Metodo del oy
-function crearTarea(event) {
-    if(inputText.value != "") {
-        return crearFlexItem();
-    } 
+let pTasks = 0, cTasks = 0;
+
+addButton.addEventListener('click', createTask);
+
+
+function createTask(event) {
+    if (inputText.value != "") {
+        return createFlexItem();
+    }
 
     return alert("Tienes que ingresar una tarea.");
 }
 
-// Metodo para crear flex items
-const crearFlexItem = () => {
-    // Contenedor del flex item
+const createFlexItem = () => {
     const divItemContainer = document.createElement("DIV");
     divItemContainer.classList.add("container__f-item");
 
-    // Contenedor del checkbox y la tarea
     const divItemContainerTitleInput = document.createElement("DIV");
     divItemContainerTitleInput.classList.add("container__f-item-task-input");
 
-    // Checkbox
     const inputCheckBox = document.createElement("INPUT");
     inputCheckBox.setAttribute("type", "checkbox");
     inputCheckBox.name = "#";
     inputCheckBox.value = "true";
     inputCheckBox.classList.add("container__input", "container__input--flex-item");
 
-    // Tarea
+    inputCheckBox.addEventListener('click', controlTasks);
+
     const taskText = document.createElement("P");
     taskText.innerHTML = inputText.value;
     taskText.classList.add("container__p");
 
-    // Boton eliminar
     const deleteButton = document.createElement("BUTTON");
     deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
     deleteButton.classList.add("container__button", "container__button--delete");
+
+    deleteButton.addEventListener('click', removeTask);
 
     divItemContainerTitleInput.append(inputCheckBox, taskText);
 
     divItemContainer.append(divItemContainerTitleInput, deleteButton);
 
     inputText.value = "";
+    addPendingTasks();
     fatherDiv.appendChild(divItemContainer);
+}
+
+// Control of complete and pending tasks
+const controlTasks = (event) => {
+    if (event.target.checked) {
+        return addCompleteTasks();
+    }
+
+    return removeCompleteTasks();
+}
+
+// Remove Task
+const removeTask = (event) => {
+    event.target.parentElement.parentElement.remove();
+    removeCompleteTasks();
+    removePendingTasks();
+
+}
+
+// Add and Remove pending tasks
+const addPendingTasks = () => {
+    pTasks++;
+    pendingTasks.innerHTML = `Tareas pendientes ${pTasks}`
+}
+const removePendingTasks = () => {
+    if (pTasks != 0) {
+        pTasks--;
+        pendingTasks.innerHTML = `Tareas pendientes ${pTasks}`
+    }
+}
+
+// Add and Remove complete tasks
+const addCompleteTasks = () => {
+    cTasks++;
+    completeTasks.innerHTML = `Completadas ${cTasks}`
+}
+
+const removeCompleteTasks = () => {
+    if (cTasks != 0) {
+        console.log("me ejecuto")
+        cTasks--;
+        completeTasks.innerHTML = `Completadas ${cTasks}`
+    }
+
 }
